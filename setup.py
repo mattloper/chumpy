b5c7aa7e36ed8987722f5f5dbe4ae051e893fdfc
Talha Zaman <talha.zaman@tuebingen.mpg.de>
2017-08-03 16:34:30 +0200

    get version without importing chumpy

--- setup.py
+++ setup.py
@@ -11,8 +11,13 @@ from pip.req import parse_requirements
 install_reqs = parse_requirements('requirements.txt', session=False)
 install_requires = [str(ir.req) for ir in install_reqs]
 
+def get_version():
+    namespace = {}
+    execfile('chumpy/version.py', namespace)
+    return namespace['version']
+
 setup(name='chumpy',
-    version=importlib.import_module('chumpy').__version__,
+    version=get_version(),
     packages = ['chumpy'],
     author='Matthew Loper',
     author_email='matt.loper@gmail.com',
